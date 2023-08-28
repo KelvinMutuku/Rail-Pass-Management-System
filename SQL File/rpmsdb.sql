14f2b648986aaec253d6aba2fe035adfb2aaff75
SE. kelvin <71261868+kelvin-thegreat@users.noreply.github.com>
2023-08-28 15:33:00 +0300

    Uploaded SQLFile

--- SQL File/rpmsdb.sql
+++ SQL File/rpmsdb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 30, 2023 at 03:02 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: Aug 28, 2023 at 02:30 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -89,9 +89,9 @@ CREATE TABLE `tblcontact` (
 --
 
 INSERT INTO `tblcontact` (`ID`, `Name`, `Email`, `Message`, `EnquiryDate`, `IsRead`) VALUES
-(1, 'rakesh kumar', 'rakesh@gmail.com', 'hfgghfghf', '2023-03-28 06:31:26', 1),
+(1, 'Solomon Vandy', 'solov@gmail.com', 'hfgghfghf', '2023-03-28 06:31:26', 1),
 (2, 'Test', 'test@gmail.com', 'sample message', '2023-03-28 06:32:55', 1),
-(3, 'Anuj', 'ak@gmail.com', 'This is testing purpose', '2023-03-30 12:20:30', 1);
+(3, 'Dr. Keith', 'drkeith@gmail.com', 'This is testing purpose', '2023-03-30 12:20:30', 1);
 
 -- --------------------------------------------------------
 
@@ -114,8 +114,8 @@ CREATE TABLE `tblpage` (
 --
 
 INSERT INTO `tblpage` (`ID`, `PageType`, `PageTitle`, `PageDescription`, `Email`, `MobileNumber`, `UpdationDate`) VALUES
-(1, 'aboutus', 'About us', '<font color=\"#747474\" face=\"Roboto, sans-serif, arial\"><span style=\"font-size: 13px;\"><b>The rail pass management system is a web-based application. This application developed in PHP using MySQLi extension.</b></span></font>', NULL, NULL, '2023-03-30 12:19:41'),
-(2, 'contactus', 'Contact Us', '                New Delhi Railway Station', 'infotest@gmail.com', 4654789799, '2023-03-30 12:20:10');
+(1, 'aboutus', 'About us', '<div style=\"text-align: left;\"><b style=\"font-size: 13px; color: rgb(116, 116, 116); font-family: Roboto, sans-serif, arial;\">The rail pass management system is a web-based application. It&nbsp;</b><font color=\"#747474\" face=\"Roboto, sans-serif, arial\"><span style=\"font-size: 13px;\"><b>streamlines rail pass and ticket management for passengers and operators. Passengers easily buy, customize and reserve passes. Real-time updates and electronic pass validation improve the journey experience. Furthermore, it enhances efficiency, revenue and sustainability, making rail travel more convenient and eco-friendly.</b></span></font></div>', NULL, NULL, '2023-08-28 12:09:36'),
+(2, 'contactus', 'Contact Us', '                Nairobi Railway Station', 'nbirailwaystation@gmail.com', 254714820808, '2023-08-28 12:22:55');
 
 -- --------------------------------------------------------
 
@@ -148,9 +148,9 @@ CREATE TABLE `tblpass` (
 --
 
 INSERT INTO `tblpass` (`ID`, `PassNumber`, `FullName`, `ProfileImage`, `ContactNumber`, `Email`, `IdentityType`, `IdentityCardno`, `Category`, `Source`, `Destination`, `TrainClass`, `FromDate`, `ToDate`, `wayType`, `Cost`, `PasscreationDate`) VALUES
-(1, '984463487', 'Mayank Tripathi', 'fc5bf5c9948c416f7c1046c8f91ba9a91679925964.png', 6446465464, 'may@gmail.com', 'Adhar Card', 'uyuiy78979789', 'Pass On Privilege Account', 'Delhi', 'Jaipur', 'II Class', '2023-03-28', '2023-04-27', 'Single Way', '200', '2023-03-27 14:06:04'),
-(2, '837149403', 'Kanika Jha', '3dfb1c8dbdcc05745b5fefc573a2a85f1679980708.png', 8979797979, 'kanika@gmail.com', 'Adhar Card', 'ui7894ko45', 'Pass On Privilege Account', 'Delhi', 'Chandigarh', 'Slepper', '2023-03-28', '2023-04-30', 'Two Way', '1500', '2023-03-28 05:18:28'),
-(3, '305788314', 'Ram Singh', '779b7513263ef185b6d094af290ef5401680178696.png', 785412600, 'rkm@test.com', 'Voter Card', 'NHJB4342485', 'Senior Citizens', 'Aligarh', 'Ghaziabad', 'General', '2023-04-01', '2023-10-31', 'Two Way', '1500', '2023-03-30 12:18:16');
+(1, '984463487', 'Kelvin Ouma', 'fc5bf5c9948c416f7c1046c8f91ba9a91679925964.png', 6446465464, 'ouma@gmail.com', 'visa Card', 'uyuiy78979789', 'Pass On Privilege Account', 'Nairobi Pipeline', 'Uganda', 'II Class', '2023-03-28', '2023-04-27', 'Single Way', 200, '2023-03-27 14:06:04'),
+(2, '837149403', 'Winnie Mwangi', '3dfb1c8dbdcc05745b5fefc573a2a85f1679980708.png', 8979797979, 'mwangiw@gmail.com', 'Master Card', 'ui7894ko45', 'Pass On Privilege Account', 'Kisumu', 'Mombasa', 'Slepper', '2023-03-28', '2023-04-30', 'Two Way', 1500, '2023-03-28 05:18:28'),
+(3, '305788314', 'Kelvin Mutuku', '779b7513263ef185b6d094af290ef5401680178696.png', 785412600, 'km@test.com', 'Paypal', 'NHJB4342485', 'Senior Citizens', 'Nairobi', 'Chad', 'General', '2023-04-01', '2023-10-31', 'Two Way', 1500, '2023-03-30 12:18:16');
 
 --
 -- Indexes for dumped tables
